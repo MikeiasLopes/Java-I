9b4ed792a9ab631746a87d3ea7542d418667ab37
Mikeias Lopes <mikeiaslopes10@gmail.com>
2025-02-05 20:32:45 -0300

    Primeiro Codigo

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a, b;
+        a = 1;
+        b = 7;
+        int soma = a + b;
+        System.out.println(soma);
+    }
+}
